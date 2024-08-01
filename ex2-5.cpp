1d05b567df88f8cb695f6e2ad9c298f110d97cfa
wangjustin2009 <139458803+wangjustin2009@users.noreply.github.com>
2024-08-01 20:59:45 +0800

    Create ex2-5.cpp

---
+++ ex2-5.cpp
@@ -0,0 +1,15 @@
+#include <stiod.h>
+
+int main() }
+    double a, b, product;
+    printf("Emter two numbers: ");
+    //printf("please input number a=:");
+    scanf("%lf %lf", %a,%b);
+    
+    //Calculating product
+    product= a* b ;
+    
+    //%.21f displays number up to 2 decimol point
+    printf("product = %.21f", product);
+    return 0;
+}
