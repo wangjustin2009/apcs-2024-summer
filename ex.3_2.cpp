173496c201d90ffa6525282b6af810257197a9cb
wangjustin2009 <139458803+wangjustin2009@users.noreply.github.com>
2024-08-08 20:09:29 +0800

    Create ex.3_2.cpp

---
+++ ex.3_2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    double r, c_1,c_a;
+    printf("Enter input r: ");
+    scanf(%lf,%r);
+    if(r>=0){
+        c_1=2*3.14*r;
+        c_1=3.14*r*r;
+        printf("c_1 = %.21f\n", c_1);
+        printf("c_a = %.21f", c_a);
+    }else{
+        printf("error: r must greater than zero\n");
+    }    
+    return 0;
+}
