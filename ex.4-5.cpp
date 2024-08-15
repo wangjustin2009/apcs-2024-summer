66d372f60cf560b3f944286a732ef60fa1aeb1b2
wangjustin2009 <139458803+wangjustin2009@users.noreply.github.com>
2024-08-15 20:23:19 +0800

    Create ex.4-5.cpp

---
+++ ex.4-5.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+   int i_num=4;
+   double f_num = 4.0;
+   f_num = pow(8.0,i_num );
+   printf(" %f \n",f_num);
+   printf("Square root of %lf is %lf \n",4.0,sqrt(4.0));
+   printf("Square root of %lf is %lf \n",5.0,sqrt(5.0));
+   
+   printf("Square root of %lf is %lf \n",4.0,f_num);
+    return 0;
+}
