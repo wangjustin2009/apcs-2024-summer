584cf4498e4db6a290ad0ac382e81b97d69364db
wangjustin2009 <139458803+wangjustin2009@users.noreply.github.com>
2024-08-29 19:19:32 +0800

    Create ex6-1.cpp

---
+++ ex6-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int *i,ii;
+    
+ii = 3 ;
+i = &ii;
+printf("\n *i = %d \n", *i);
+printf(" &ii = %x \n",&ii);
+printf(" i = %x  \n", i);
+    return 0;
+}
